79124c788b2a67979c763867d4d4a3e69dfa785f
Ayush Yadav <ayushyadav935@gmail.com>
2021-12-03 00:27:07 +0530

    Redux store setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { useEffect } from 'react';
 import './App.css';
 import dbRef,{userName,connectedRef} from './server/firebase';
+import { connect } from "react-redux";
+import { setUser,addParticipant,removeParticipant } from './store/actionCreator';
 
-function App() {
+function App(props) {
+  const participantsRef = dbRef.child("participants")
+
   useEffect(() => {
-    const participantsRef = dbRef.child("participants")
     connectedRef.on("value",(snap)=>{
       if(snap.val()){
         const defaultPref = {
@@ -16,16 +19,60 @@ function App() {
           userName,
           preference:defaultPref 
         })
+        props.setUser({
+          [userRef.key]:{
+            userName,
+            ...defaultPref
+          }
+        })
         userRef.onDisconnect().remove()
       }
     })
+    // eslint-disable-next-line
   }, [])
+
+  useEffect(() => {
+    if(props.user){
+      console.log(props.user)
+      participantsRef.on("child_added",(snap)=>{
+        const {userName,preference} = snap.val()
+        props.addParticipant({
+          [snap.key]:{
+            userName,
+            ...preference
+          }
+        })
+      })
+  
+      participantsRef.on("child_removed",(snap)=>{
+        props.removeParticipant(snap.key)
+      })
+    }
+     // eslint-disable-next-line
+  }, [props.user])
 
   return (
     <div>
-      {userName}
+      User: {JSON.stringify(props.user)}<br/>
+      Participants: {JSON.stringify(props.participants)}
     </div>
   );
-}
+  }
 
-export default App;
+  const mapStateToProps = (state) => {
+    return {
+      user: state.currentUser,
+      participants: state.participants
+    };
+  };
+
+  const mapDispatchToProps = (dispatch) => {
+    return {
+      setUser: (user) => dispatch(setUser(user)),
+      addParticipant: (participant) => dispatch(addParticipant(participant)),
+      removeParticipant: (participantKey) => dispatch(removeParticipant(participantKey)),
+    };
+  };
+
+
+  export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from "react-redux";
+import { createStore,applyMiddleware,compose  } from "redux";
+import thunk from "redux-thunk";
+import { reducer } from "./store/reducers";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(reducer,{},composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
